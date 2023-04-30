f401732a85bcb82865429076a65ab0b45aa6d135
Caarlos94 <carlos94_cr7@hotmail.com>
2023-04-30 15:11:38 -0700

    para revision

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ btn1.onclick = function(e) {
   e.preventDefault();
   if(res1[0].checked || res1[1].checked || res1[2].checked || res1[3].checked) {
     p1.style.display = 'none';
-    p2.style.display = 'block';
+    p11.style.display = 'block';
   } 
 }
 
